28df981b568f5e8d673102290ed67208b5529a68
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-05-31 21:54:13 -0400

    Mount express.static middleware to the path /public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   path = __dirname + "/views/index.html";
   res.sendFile(path);
