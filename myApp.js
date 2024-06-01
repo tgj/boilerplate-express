77cc91e253e0663447e1f854207d74f3283e1353
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-05-31 23:35:14 -0400

    add /name endpoint

--- myApp.js
+++ myApp.js
@@ -46,4 +46,10 @@ app.get("/:word/echo", (req, res) => {
   });
 });
 
+app.get("/name", (req, res) => {
+  res.json({
+    name: `${req.query.first} ${req.query.last}`,
+  });
+});
+
 module.exports = app;
