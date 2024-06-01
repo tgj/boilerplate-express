86875495991a592e2144eaad0e13b081f744766f
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-05-31 23:15:59 -0400

    Add /now middleware

--- myApp.js
+++ myApp.js
@@ -27,4 +27,11 @@ app.get("/json", (req, res) => {
   });
 });
 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  res.json({
+    time: req.time,
+  });
+});
+
 module.exports = app;
